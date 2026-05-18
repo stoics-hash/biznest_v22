94b90dd3ef257baaff236595c2b70d571ce42827
Jetross <jetrossneri07@gmail.com>
2026-05-18 20:07:04 +0800

    feat: add mode-toggle

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,3 +1,13 @@
-export default function App() {
-  return null
+import type { PropsWithChildren } from "react";
+
+import { ThemeProvider } from "./components/ui/theme-provider";
+
+function App({ children }: PropsWithChildren) {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
+  );
 }
+
+export default App;
